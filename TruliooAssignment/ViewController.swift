835c3aa9f83c8bc673790169b1bcd72ec1ac7fdf
Roni Leshes <ronileshes@gmail.com>
2023-03-12 15:58:15 -0700

    Initial Commit

---
+++ TruliooAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TruliooAssignment
+//
+//  Created by Roni Leshes on 12/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
